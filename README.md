e590eb131e19174599181636ef54afd2085ec1b7
Abdul Ahad <abdul.ahad@brainstation-23.com>
2024-02-10 22:42:45 +0600

    Add initial Django files and configurations

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ramadan Planner Backend
+This is the backend for the Ramadan Planner app. It is built using Django and Django Ninja.
+
+
+
+
---
+++ account/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from account.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    A class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Email and password are required. Other fields are optional.
+    """
+
+    email = models.EmailField(_("email address"), unique=True, null=False)
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, null=True, blank=True)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
