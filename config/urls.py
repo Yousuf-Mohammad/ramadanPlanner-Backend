56c5aaf19e6d905fbc4a41a5f968986250f994fe
Abdul Ahad <abdul.ahad@brainstation-23.com>
2024-02-15 01:18:17 +0600

    remove unnecessary files

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from ninja_extra import NinjaExtraAPI
@@ -29,9 +27,3 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
-
-if settings.DEBUG:
-
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )  # type: ignore
