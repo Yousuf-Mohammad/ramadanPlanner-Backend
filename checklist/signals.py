89e4c208ef392e3938afface657ebcd6eb866f0f
Abdul Ahad <abdul.ahad@brainstation-23.com>
2024-02-23 23:05:54 +0600

    add signals to add all default item to checklists

---
+++ checklist/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from checklist.models import ChecklistItem, DailyActivityChecklist, DefaultChecklistItem
+
+
+@receiver(post_save, sender=DailyActivityChecklist)
+def create_default_checklist_items(sender, instance, created, **kwargs):
+    if created:
+        for default_item in DefaultChecklistItem.objects.all():
+            ChecklistItem.objects.create(
+                daily_activity_checklist=instance, default_item=default_item
+            )
